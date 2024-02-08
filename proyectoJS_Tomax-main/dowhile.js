6ab4ca2a4c69223e41812086142d17fb56a60ed7
Tomax-gomez <127521420+Tomax-gomez@users.noreply.github.com>
2024-02-08 14:32:45 -0500

    Add files via upload

---
+++ proyectoJS_Tomax-main/dowhile.js
@@ -0,0 +1,11 @@
+
+
+let refill = 3;
+do{
+  alert("rellenar vasos" + refill);
+  
+  if(refill== 0){
+    alert("no te")
+  }
+  refill --;
+}while(refill);
---
+++ proyectoJS_Tomax-main/operadorigual.js
@@ -0,0 +1,9 @@
+
+
+let fill =3;
+
+
+let refill =fill;
+
+console.log(fill==refill);
+console.log(fill===refill);
